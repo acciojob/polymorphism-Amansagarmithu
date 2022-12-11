7a1a8a014afe972c18a7072af5c9a76bd81d106d
Aman Sagar <amansagar887@gmail.com>
2022-12-11 20:46:52 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
+
 package com.driver;
-
+import java.util.*;
 public class Main {
-
+    public static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Product P = new Product();
+        P.product(input.nextInt(),input.nextInt());
+        P.product(input.nextInt(),input.nextInt(),input.nextInt());
+        P.product(input.nextDouble(),input.nextDouble());
+    }
 }
